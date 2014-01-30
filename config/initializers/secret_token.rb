0c362106ecdeca3a4d3c9c73da9b2ad09e0299cc
Róbert Viðar Bjarnason <robert@citizens.is>
2014-01-30 10:13:46 +0000

    Secret token in ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -3,5 +3,12 @@
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-OpenActiveVotingEditor::Application.config.secret_token = 'a8ff62b5384ee97f505bed9742742430d17e9cae1f258aa3ec188c81d687b09b94ba3b2bfdf062851761f6c3b596906838fa950cf52c338396c8d5549b10448a'
+# no regular words or you'll be exposed to dictionary attacks
+#
+# example token: a8ff62b5384ee97f508bed9742742430d16e9cae1f258aa3ec188a81d687b09b94ba3b2bfdf062851761f6c3b596906838fa950cf52c338396c8d5549b10448a
+
+if ENV['RAILS_SECRET_TOKEN']
+  OpenActiveVotingEditor::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
+else
+  raise "ENV['RAILS_SECRET_TOKEN'] is missing"
+end
